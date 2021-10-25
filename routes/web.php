f1f66ea1651a8f309399a10cb434f072eeeb4cc2
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-10-25 23:13:57 +0800

    每一個 Route 請對應到一個獨立的 Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('about', [AboutController::class, 'index'])->name('about.index');
+Route::get('news', [NewsController::class, 'index'])->name('news.index');
